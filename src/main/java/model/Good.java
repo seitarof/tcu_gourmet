0ee995390f0ccddfa4772fe4f6ea7a638214d59e
DreNo <dreno@abura.gitgit>
2021-06-25 23:28:42 +0900

    ver1.0-ひとまず完成

---
+++ src/main/java/model/Good.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Good {
+    private int goodID;
+private int postID;
+private int userID;
+private int goodCount;
+public void setGoodID(int goodID){
+    this.goodID = goodID;
+}
+public int getGoodID(){
+    return this.goodID;
+}
+public void setPostID(int postID){
+    this.postID = postID;
+}
+public int getPostID(){
+    return postID;
+}
+public void setUserID(int userID){
+    this.userID = userID;
+}
+public int getUserID(){
+    return userID;
+}
+public void setGoodCount(int goodCount){
+    this.goodCount = goodCount;
+}
+public void plusGoodCount(){
+    this.goodCount++;
+}
+public int getGoodCount(){
+    return goodCount;
+}
+    
+}
