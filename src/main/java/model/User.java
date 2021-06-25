304b3d431646736565f3caf7d0de1b8ede6276e9
DreNo <dreno@abura.gitgit>
2021-06-25 22:57:15 +0900

    ver.2.0-暫定的な完成

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,30 +1,43 @@
 package model;
 
+import java.io.Serializable;
+import java.security.MessageDigest;
 import java.util.Scanner;
+import java.math.BigInteger;
 
-public class User {
+public class User implements Serializable{
     private String name;
     private int userID;
     private String hashedPass;
     private boolean admin = false;
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return this.name;
     }
-    public void setUserID(int userID){
+
+    public void setUserID(int userID) {
         this.userID = userID;
     }
-    public int getUserID(){
+
+    public int getUserID() {
         return this.userID;
     }
-    public void setHashedPass(){
+
+    public void setHashedPass() {
         Scanner scanner = new Scanner(System.in);
         String pass = scanner.nextLine();
-        
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] result = digest.digest(pass.getBytes());
+            this.hashedPass = String.format("%040x", new BigInteger(1, result));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-
-    
 }
