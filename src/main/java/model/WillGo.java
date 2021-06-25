3f8b9d59bb6fb2bcb8b6d804ec1f9cb4d297b14a
DreNo <dreno@abura.gitgit>
2021-06-25 23:41:43 +0900

    ver.1.0-ひとまず完成

---
+++ src/main/java/model/WillGo.java
@@ -0,0 +1,41 @@
+package model;
+
+public class WillGo {
+    private int willGoID;
+    private int postID;
+    private int userID;
+    private int willGoDate;
+
+    public void WillGoID(int willGoID) {
+        this.willGoID = willGoID;
+    }
+
+    public int getWillGoID() {
+        return this.willGoID;
+    }
+
+    public void setPostID(int postID) {
+        this.postID = postID;
+    }
+
+    public int getPostID() {
+        return this.postID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
+
+    public void setWillGoDate(int willGoDate) {
+        this.willGoDate = willGoDate;
+    }
+
+    public int getWillGoDate() {
+        return this.willGoDate;
+    }
+
+}
