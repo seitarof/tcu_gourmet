2fea1cbec30861f398aa4c7d5e4ffd7d75c59603
sfujigaki <fujigaki@sec.inf.shizuoka.ac.jp>
2021-06-28 11:35:02 +0900

    Update UserDAO

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -41,4 +41,82 @@ public class UserDAO implements DAO {
 		return userList;
 	}
 
+	public Boolean createUser(User user) {
+		try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+			
+			// INSERT文を準備
+			String sql = "INSERT INTO USER(NAME, PASS, EMAIL, ADMIN) VALUES(?, ?, ?, false)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			// INSERT文中の「？」に使用する値を設定しSQLを完成
+			pStmt.setString(1, user.getName());
+			pStmt.setString(2, user.getHashedPass());
+			pStmt.setString(3, user.getEmail());
+			
+			//INSERTを実行
+			int result = pStmt.executeUpdate();
+			if(result != 1) {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public User readUser(String emailString) {
+		User user = null;
+		// DBへ接続
+		try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+			
+			// SELECT文を準備
+			String sql = "SELECT USER_ID, NAME, PASS, EMAIL, ADMIN FROM USER WHERE EMAIL = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, emailString);
+			
+			//SELECTを実行し、結果表を取得
+			ResultSet rs = pStmt.executeQuery();
+			
+			// 結果表に格納されたレコードの内容をUserインスタンスに設定し、ArrayListインスタンスに追加
+			while (rs.next()) {
+				int userID = rs.getInt("USER_ID");
+				String name = rs.getString("NAME");
+				String pass = rs.getString("PASS");
+				String email = rs.getString("EMAIL");
+				Boolean admin = rs.getBoolean("ADIMN");
+				user = new User(userID, name, pass, email, admin);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return user;
+	}
+	
+	public Boolean updateUser(String name, String pass, String email, String admin) {
+		// TODO 更新メソッドを書く
+		return false;
+	}
+	
+	public Boolean deleteUser(String emailString) {
+		// DBへ接続
+		try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+			
+			// DELETE文を準備
+			String sql = "DELETE FROM USER WHERE EMAIL = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, emailString);
+			
+			//DELETEを実行し、結果表を取得
+			int result = pStmt.executeUpdate();
+			if(result != 1) {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 }
